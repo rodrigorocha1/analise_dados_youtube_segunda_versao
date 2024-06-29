d6e9f5228a9d3f91f0fa5e5d5c0dbc6f1745e749
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-06-29 15:52:07 -0300

    correção

--- hook/youtube_busca_canais_hook.py
+++ hook/youtube_busca_canais_hook.py
@@ -4,5 +4,27 @@ import variaveis.variaveis as v
 
 
 class YoutubeBuscaCanaisHook(YoutubeHook):
-    def __init__(self, data_inicio: str, consulta: str = None, conn_id: str = None, carregar_dados: IInfraDados = None) -> None:
-        super().__init__(data_inicio, consulta, conn_id, carregar_dados)
+    def __init__(self, conn_id: str = None, carregar_dados: IInfraDados = None) -> None:
+        super().__init__(conn_id, carregar_dados)
+
+    def _criar_url(self):
+        return self._URL + '/channels/'
+
+    def run(self):
+        session = self.get_conn()
+        lista_canais = self._carregar_dados.carregar_dados()
+        url = self._criar_url()
+        params = [
+            {
+                'part': 'snippet,statistics',
+                'id': id_canal,
+                'key': v.chave_youtube,
+
+            } for id_canal in lista_canais
+        ]
+        response = self._executar_paginacao(
+            url=url,
+            session=session,
+            params=params
+        )
+        return response
--- operators/youtube_busca_canais_operator.py
+++ operators/youtube_busca_canais_operator.py
@@ -3,5 +3,9 @@ from operators.youtube_operator import YoutubeOperator
 
 
 class YoutubeBuscaCanaisOperator(YoutubeOperator):
+    template_fields = [
+        'ordem_extracao'
+    ]
+
     def __init__(self, ordem_extracao: YoutubeHook, **kwargs):
         super().__init__(ordem_extracao, **kwargs)
