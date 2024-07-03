99270fea7d6aa7919e51bbd426eafeac3632e442
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-07-02 22:24:00 -0300

    formatação etl spark

--- spark_etl/transform.py
+++ spark_etl/transform.py
@@ -1,15 +1,39 @@
 import os
+from typing import List, Dict
 from pyspark.sql import SparkSession, DataFrame
 import pyspark.sql.functions as f
 
 
-def abrir_dataframe(spark: SparkSession) -> DataFrame:
-    dataframe = spark.read.json(
-        '../datalake/bronze/cities_skylines/extracao_data_2024_06_29/estatisticas_canais_brasileiros/req_estatisticas_canais_brasileiros.json')
+def abrir_dataframe(spark: SparkSession, camada_datalake: str, extracao_data: str, metrica: str, arquivo_json: str) -> DataFrame:
+    """Função para abrir o dataframe
+
+    Args:
+        spark (SparkSession): Sessão Spark      
+        camada_datalake (str): Camada datalake (bronze, prata, ouro)
+        extracao_data (str): diretório com a pasta com o nome da extração ex: extracao_data_2024_07_02_15_05_38
+        metrica (str): métrica Ex. estatisticas_canais_brasileiros, estatistica videos
+        arquivo_json (str): nome do arquivo json
+
+    Returns:
+        DataFrame: Dataframe sparl
+    """
+    CAMINHO_RAIZ = os.getcwd()
+
+    dataframe = spark.read.json(os.path.join(
+        CAMINHO_RAIZ, 'datalake', camada_datalake, extracao_data, metrica, arquivo_json))
+
     return dataframe
 
 
 def obter_hora(dataframe: DataFrame) -> DataFrame:
+    """função para converter hora
+
+    Args:
+        dataframe (DataFrame): Dataframe pyspark
+
+    Returns:
+        DataFrame: Retorna um dataframe
+    """
     dataframe = dataframe.withColumn('TURNO_EXTRACAO', f.when((f.col('HORA') >= 0) & (f.col('HORA') < 6), 'Madrugada')
                                      .when((f.col('HORA') >= 6) & (f.col('HORA') < 12), 'Manhã')
                                      .when((f.col('HORA') >= 12) & (f.col('HORA') < 18), 'Tarde')
@@ -18,6 +42,14 @@ def obter_hora(dataframe: DataFrame) -> DataFrame:
 
 
 def transformar_estatisticas_canais(dataframe: DataFrame) -> DataFrame:
+    """transformar_estatisticas_canais
+
+    Args:
+        dataframe (DataFrame): Dataframe pyspark
+
+    Returns:
+        DataFrame: Retorna um dataframe
+    """
     dataframe = dataframe.select(
         'data_extracao',
         f.explode('items').alias('item')
@@ -37,10 +69,19 @@ def transformar_estatisticas_canais(dataframe: DataFrame) -> DataFrame:
 
 
 def transform_estatisticas_videos(df_video_json: DataFrame) -> DataFrame:
-    df_video_json = df_video_json.select(
+    """transform_estatisticas_videos
+
+    Args:
+        df_video_json (DataFrame): Dataframe
+
+    Returns:
+        DataFrame: Dataframe
+    """
+    df_video_json = df_video_json.withColumn('HORA', f.hour('data_extracao')).select(
         'data_extracao',
         f.col('items.id').alias('ID_VIDEO'),
         f.col('items.snippet.publishedAt').alias('DATA_PUBLICACAO'),
+        f.col('HORA'),
         f.col('items.snippet.channelId').alias('ID_CANAL'),
         f.col('items.snippet.channelTitle').alias('NM_CANAL'),
         f.col('items.snippet.categoryId').alias('ID_CATEGORIA'),
@@ -57,14 +98,23 @@ def transform_estatisticas_videos(df_video_json: DataFrame) -> DataFrame:
 
 
 def transform_estatisticas_videos_trends(df_trend_brazil: DataFrame) -> DataFrame:
+    """transform_estatisticas_videos_trends
+
+    Args:
+        df_trend_brazil (DataFrame): DataFrame
+
+    Returns:
+        DataFrame: DataFrame
+    """
     df_trend_brazil = df_trend_brazil.drop('_corrupt_record')
     df_trend_brazil = df_trend_brazil.na.drop('all')
-    df_trend_brazil = df_trend_brazil.select(
+    df_trend_brazil = df_trend_brazil.withColumn('HORA', f.hour('data_extracao')).select(
         'data_extracao',
         f.explode('items')
     )
     df_trend_brazil = df_trend_brazil.select(
         'data_extracao',
+        f.col('HORA'),
         f.col('col.id'),
         f.col('col.contentDetails.*'),
         f.col('col.snippet.*'),
@@ -86,12 +136,36 @@ def transform_estatisticas_videos_trends(df_trend_brazil: DataFrame) -> DataFram
     return df_trend_brazil
 
 
-def save_parquet(df_json: DataFrame, diretorio_salvar: str):
-    df_json.coalesce(1) \
-        .write \
-        .mode('overwrite')\
-        .parquet(diretorio_salvar)
+def save_parquet(dataframe: DataFrame, assunto: str, extracao_data: str, metrica: str, nome_arquivo: str):
+    """Salvar o arquivo parquet na camada prata
+
+    Args:
+        dataframe (DataFrame): Dataframe do pandas
+        assunto (str): assunto da pesquisa
+        extracao_data (str): diretório com a pasta com o nome da extração ex: extracao_data_2024_07_02_15_05_38
+        metrica (str): métrica Ex. estatisticas_canais_brasileiros, estatistica videos
+        nome_arquivo (str): nome do arquivo a ser salvo no parquet ex: teste.parquet
+    """
+    CAMINHO_RAIZ = os.getcwd()
+    dataframe.toPandas().to_parquet(path=os.path.join(
+        CAMINHO_RAIZ, 'datalake', 'prata', assunto, extracao_data, metrica, nome_arquivo))
+
+    # df_json.coalesce(1) \
+    #     .write \
+    #     .mode('overwrite')\
+    #     .parquet(diretorio_salvar)
 
 
-if __name__ == '___main__':
-    pass
+def realizar_etl(spark: SparkSession, caminhos_datalake: List[Dict[str, str]]):
+
+    for caminho_datalake in caminhos_datalake:
+        caminho_datalake['spark'] = spark
+        dataframe = abrir_dataframe(**caminho_datalake)
+        if caminho_datalake['metrica'] == 'estatisticas_canais_brasileiros':
+            dataframe = transformar_estatisticas_canais(dataframe=dataframe)
+        else:
+            dataframe = transform_estatisticas_videos(df_video_json=dataframe)
+
+        dataframe = obter_hora(dataframe=dataframe)
+        caminho_datalake['dataframe'] = dataframe
+        save_parquet(**caminho_datalake)
