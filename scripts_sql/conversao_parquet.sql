41782e6e093887af7ac15591bcb77fabadff1e8d
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-07-03 22:29:40 -0300

    estrutura da dag de camada ouro

---
+++ scripts_sql/conversao_parquet.sql
@@ -0,0 +1,6 @@
+
+COPY (SELECT *
+from read_parquet(
+'/home/rodrigo/Documentos/projetos/analise_dados_youtube_segunda_versao/datalake/prata/cities_skylines/extracao_data_2024_06_29/estatisticas_canais_brasileiros/req_estatisticas_canais_brasileiros.parquet')
+)
+TO '/home/rodrigo/Documentos/projetos/analise_dados_youtube_segunda_versao/datalake/ouro/estatisticas_canais_brasileiros.parquet' (FORMAT PARQUET);
